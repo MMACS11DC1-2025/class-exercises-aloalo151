cc3fef18934cfd9bada948a3830970c2292bd567
aloalo151 <nguyen.ddk274@gmail.com>
2025-11-28 13:35:36 -0800

    started on function to judge ripeness - will be used for every img and get ripe percentage to a bigger list

--- 6.7/main.py
+++ 6.7/main.py
@@ -1,5 +1,20 @@
 '''
 Banana Checker:
 Looks at the colour of the banana and judge its ripeness
+green = unripe, green-yellow = just ripe, yellow = ripe, yellow-brown = too ripe, brownish = overipe
 '''
 
+from PIL import Image
+
+imgList = []
+
+def judgeRipeness(img):
+    width = img.width
+    height = img.height
+    pixels = []
+    
+    for x in range(width):
+        for y in range(height):
+            r, g, b = img[x, y]
+            if b <= 100:
+                pixels.append([x, y, (r, g)])
