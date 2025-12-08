3873cc63950443e0b7be114184eb2504b7b4e5d3
aloalo151 <nguyen.ddk274@gmail.com>
2025-12-08 13:34:45 -0800

    fixed labeling function (we are doing good tonight)

--- 6.7/main.py
+++ 6.7/main.py
@@ -4,6 +4,7 @@ Go through a picture of a morse code message, identify the dots and lines, then 
 '''
 
 from PIL import Image
+import random
 
 ''' 
 Pseudocode:
@@ -32,35 +33,55 @@ img = Image.open("6.7/morse_images/morse1.png").load()
 imgOutput = Image.open("6.7/morse_images/morse1.png")
 width, height = imgOutput.size
 
-for x in range(width):
+for y in range(height):
     imgLabels.append([])
-    for y in range(height):
-        imgLabels[x].append("")
+    for x in range(width):
+        imgLabels[y].append("")
         
-for x in range(width):
-    for y in range(height):
+for y in range(height):
+    for x in range(width):
         if img[x, y] == (249, 250, 251, 255):
             imgOutput.putpixel([x, y], (255, 255, 255, 255))
         else:
             imgOutput.putpixel([x, y], (0, 0, 0, 0))
         
 def labelPixel(x, y, label):
-    if imgLabels[x][y] == "":
+    if imgLabels[y][x] == "":
         r, g, b, a = img[x, y]
-        if r<249 and g<250 and b<251:
-            imgLabels[x][y] = label
+        if r<100 and g<100 and b<100:
+            imgLabels[y][x] = label
             for x_pos in range(x-1, x+2):
                 for y_pos in range(y-1, y+2):
-                    if 0 <= x_pos <= width and 0 <= y_pos <= height and (x_pos != x and y_pos != y):
+                    if 0 <= x_pos <= width and 0 <= y_pos <= height:
                         labelPixel(x_pos, y_pos, label)
             return True
         else: 
-            imgLabels[x][y] = 0
+            imgLabels[y][x] = 0
             return False
+    else:
+        return False
 
 for x in range(width):
     for y in range(height):
         if labelPixel(x, y, labelNum):
             labelNum += 1
-print(img[0, 0] == (249, 250, 251, 255))
+                   
+label1 = []
+for i in range(1, labelNum):
+    colour = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+    for y in range(height):
+        for x in range(width):
+            if imgLabels[y][x] == i:
+                imgOutput.putpixel([x, y], (colour))
+            
+
+
+# for y in range(height):
+#     for x in range(width):
+#         if imgLabels[y][x] != 0:
+#             imgOutput.putpixel([x, y], (imgLabels[y][x], 0, 0))
+            
+# for y in range(height):
+#     print(imgLabels[y])
 imgOutput.save("image.png")
+print(labelNum)
